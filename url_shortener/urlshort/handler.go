acd9f58b48096172b7e0dfde12c8e6a5b3983c14
raszewski_rus <bear27098@gmail.com>
2022-08-18 02:13:27 +0300

    test

--- url_shortener/urlshort/handler.go
+++ url_shortener/urlshort/handler.go
@@ -14,10 +14,10 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 		if dest, ok := pathsToUrls[path]; ok {
-			http.Redirect(w,r,dest,http.StausFound)
+			http.Redirect(w,r,dest,http.StatusFound)
 			return
 		}
-		fallback.ServerHTTP(w,r)
+		fallback.ServeHTTP(w,r)
 	}
 
 }

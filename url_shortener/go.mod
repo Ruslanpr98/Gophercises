6114b09eec20e0910e5bc34893982485a9566838
raszewski_rus <bear27098@gmail.com>
2022-08-21 19:20:11 +0300

    Map Handler

---
+++ url_shortener/go.mod
@@ -0,0 +1,7 @@
+module url_shortener
+
+replace urlshort => ./urlshort
+
+go 1.18
+
+require urlshort v0.0.0-00010101000000-000000000000
--- url_shortener/main.go
+++ url_shortener/main.go
@@ -14,20 +14,20 @@ func main() {
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
-	mapHandler := MapHandler(pathsToUrls, mux)
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-	- path: /urlshort
-	url: https://github.com/gophercises/urlshort
-	- path: /urlshort-final
-	url: https://github.com/gophercises/urlshort/tree/solution
-	`
-	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	// yaml := `
+	// - path: /urlshort
+	// url: https://github.com/gophercises/urlshort
+	// - path: /urlshort-final
+	// url: https://github.com/gophercises/urlshort/tree/solution
+	// `
+	// yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+	// if err != nil {
+	// 	panic(err)
+	// }
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", mapHandler)
 }
